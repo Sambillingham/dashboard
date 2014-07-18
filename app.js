92fd36665cfd37427880a17ae6b5b602ea66e61e
Sam Billingham <sam@harkable.com>
2014-07-18 15:52:09 +0100

    upgraded to express 4.x

--- app.js
+++ app.js
@@ -3,18 +3,27 @@
  */
 
 var express = require('express'),
-    routes = require('./routes'),
+	connect = require('connect'),
+	favicon = require('serve-favicon'),
+    morgan = require('morgan'),
+    methodOverride = require('method-override'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    errorhandler = require('errorhandler'),
     http = require('http'),
     path = require('path'),
     mongoose = require('mongoose'),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
+    session = require('express-session'),
+    MongoStore = require('connect-mongo')(session),
     flash = require('connect-flash'),
     app = express(),
-    server = http.createServer(app),
-    MongoStore = require('connect-mongo')(express);
+    server = http.createServer(app);
 
-    var config = require('./config');
+    //include moules
+    var routes = require('./routes'),
+    	config = require('./config');
 
 // mongoose
 var databaseName = config.database;
@@ -30,29 +39,33 @@ app.sessionStore = new MongoStore({ url: 'mongodb://localhost/' + databaseName }
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your super secret something here'));
-app.use(express.session({
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json())
+app.use(methodOverride());
+app.use(cookieParser('your super secret something here'));
+app.use(session({
     secret: config.cryptoKey,
-    store: app.sessionStore
-  }));
+    store: app.sessionStore,
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next) {
-  res.locals.user = req.user;
-  next();
+    res.locals.user = req.user;
+    next();
 });
 
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(errorhandler({ dumpExceptions: true, showStack: true }));
 }
 
 // passport config
@@ -65,5 +78,5 @@ passport.deserializeUser(Account.deserializeUser());
 routes(app);
 
 server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Dashboard - Express/Passport stuff
 ===
-A basic setup for Node and Express 3.x. Auth with passport.
+A basic setup for Node and Express 4.x. Local auth with passport.
 
 if you're after something like this but ready for production perhaps [drywall](http://jedireza.github.io/drywall/ "drywall") might work for you.
 
@@ -8,5 +8,6 @@ if you're after something like this but ready for production perhaps [drywall](h
 - Promises for mongodb calls
 
 ## Updates
+- 0.0.4 Upgraded Express from 3.x to 4.x and all added new needed dependencies
 - 0.0.3 Added basic Gulp setup
 - 0.0.2 Session Store, config file, slight app.js refactor
